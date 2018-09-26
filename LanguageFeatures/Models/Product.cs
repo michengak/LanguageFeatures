6f0ec44f21847daf6c146b02214873f2aca4ec89
michel ngako <emoumichel@gmail.com>
2018-09-26 08:23:10 -0400

    Add project files.

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+            private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public int ProductID { get; set; }
+            
+        
+                
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { set; get; }
+    }
+}
